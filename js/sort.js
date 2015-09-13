33acb80a9aae0c46932445e31b85be8c1ad64492
Denny Lüttschwager <smoxe@gmx.de>
2015-09-13 13:48:11 +0200

    Usersort umgebaut Neues Usersort mit neuen Orangenen Look angefangen

--- js/sort.js
+++ js/sort.js
@@ -45,7 +45,9 @@ function includeAll() {
     include('js/animate/animate-sort-new.js');
     include('js/animate/animate-sort-old.js');
     include('js/animate/animate-sort2.js');  
-    include('js/animate/animate-heapsort.js');    
+    include('js/animate/animate-heapsort.js');
+    include('js/animate/animate-usersort.js');
+    include('js/animate/animate-usersort-2.js');
    
 
     //Sortierverfahren
@@ -273,5 +275,13 @@ function initHeapsortTree(pSortAreaID){
      HeapsortTree = new heapsortTree(pSortAreaID);
 }
 
-
-
+/*
+ * Usersort Grafische Darstellung
+ * 
+ * initHeapsortTree
+ * Generiert die Auszugebene Klasse
+ */
+var UserSortAll = null;
+function initUserSort(pSortAreaID) {
+    UserSortAll = new UserSorting2(pSortAreaID);
+}
